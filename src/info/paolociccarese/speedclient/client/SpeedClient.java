5f215b6b1f3c0af08bb0b2f4ff5420940f23198e
Paolo Ciccarese <paolo.ciccarese@gmail.com>
2016-04-13 00:25:28 -0400

    New logging management

--- src/info/paolociccarese/speedclient/client/SpeedClient.java
+++ src/info/paolociccarese/speedclient/client/SpeedClient.java
@@ -1,16 +1,14 @@
 package info.paolociccarese.speedclient.client;
 
 import info.paolociccarese.speedapp.core.ASpeedApp;
-import info.paolociccarese.speedapp.core.logging.IAppLoggable;
-import info.paolociccarese.speedapp.core.logging.ILogger;
-import info.paolociccarese.speedapp.core.logging.InMemoryLogger;
-import info.paolociccarese.speedapp.core.logging.LogLevel;
 import info.paolociccarese.speedapp.core.ui.IAppResources;
 import info.paolociccarese.speedapp.core.ui.IResizable;
-import info.paolociccarese.speedapp.core.ui.logging.LogListPanel;
+
+import java.util.logging.Logger;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.logging.client.LogConfiguration;
 import com.google.gwt.user.client.Window;
 
 /**
@@ -21,18 +19,20 @@ public class SpeedClient extends ASpeedApp implements EntryPoint {
 	 * The static images used throughout the Domeo application.
 	 */
 	public static final IAppResources applicationResources = GWT.create(IAppResources.class);
+	private static Logger logger = Logger.getLogger("info.paolociccarese.speedclient.client.SpeedClient");
 
 	public static SpeedClient _app = null;
 
 	protected void parametrize() {
-		getConfigurer().registerPreferenceItemValue(ASpeedApp.class.getName(), IAppLoggable.LOGGER, InMemoryLogger.class.getName());
-		getConfigurer().registerPreferenceItemValue(ASpeedApp.class.getName(), ILogger.LOGGING_LEVEL, LogLevel.DEBUG.getValue());
-		getConfigurer().registerPreferenceItemValue(ASpeedApp.class.getName(), ILogger.LOGGING_PROPAGATION_LEVEL, LogLevel.INFO.getValue());
-		getConfigurer().registerPreferenceItemValue(ASpeedApp.class.getName(), LogListPanel.DISPLAY_LEVEL, LogLevel.DEBUG.getValue());
+		//getConfigurer().registerPreferenceItemValue(ASpeedApp.class.getName(), IAppLoggable.LOGGER, InMemoryLogger.class.getName());
+//		getConfigurer().registerPreferenceItemValue(ASpeedApp.class.getName(), ILogger.LOGGING_LEVEL, LogLevel.DEBUG.getValue());
+//		getConfigurer().registerPreferenceItemValue(ASpeedApp.class.getName(), ILogger.LOGGING_PROPAGATION_LEVEL, LogLevel.INFO.getValue());
+//		getConfigurer().registerPreferenceItemValue(ASpeedApp.class.getName(), LogListPanel.DISPLAY_LEVEL, LogLevel.DEBUG.getValue());
+		logger.info(".parametrize() ... none");
 	}
 	
 	protected void keys() {
-		getLogger().info(this, ".customKeys() ... none");
+		logger.info(".customKeys() ... none");
 		return;
 	}
 	
@@ -41,14 +41,18 @@ public class SpeedClient extends ASpeedApp implements EntryPoint {
 	 */
 	public void onModuleLoad() {
 		_app = this;
-		_app.initialize();
-
-		_app.getLogger().debug(this, "yolo");
-	}
-	
-	@Override
-	public ILogger customLoggers(String idLogger) {
-		return ILogger.NO_LOGGER;
+		_app.initialize("SpeedClient");
+		
+		long startTime = System.currentTimeMillis();
+		if (LogConfiguration.loggingIsEnabled()) {
+			log().jobStart("Bootstrapping", "Bootstrapping application");
+		}
+		
+		// Bootstrapping
+		
+		if (LogConfiguration.loggingIsEnabled()) {
+			log().jobEnd("Bootstrapping", "Application ready", startTime);
+		}
 	}
 
 	@Override
